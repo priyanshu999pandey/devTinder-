80ad3ad4ccf6ad66e97ec00cce7833e44eab4753
Priyanshu pandey <priyanshu999pandey@gmail.com>
2025-04-28 18:15:02 +0530

    exploring route

--- src/app.js
+++ src/app.js
@@ -2,6 +2,20 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user",(req,res)=>{
+  res.send({firstName:"priyanshu",
+    lastName:"pandey"
+  })
+})
+
+app.post("/user",(req,res)=>{
+  res.send("posted successfully!!");
+})
+
+app.delete("/user",(req,res)=>{
+ res.send("delted successfulyy!")
+})
+
 app.use("/home",(req,res)=>{
   res.send("home 99  page")
 })
