bab2350895c6ff577321508cf7d49ca47648eb5e
Priyanshu pandey <priyanshu999pandey@gmail.com>
2025-04-29 18:42:52 +0530

    get-api

--- src/app.js
+++ src/app.js
@@ -17,6 +17,38 @@ app.post("/signup",async(req,res)=>{
   }
 })
 
+// GET-USER_API: get user by email
+app.get("/user",async(req,res)=>{
+  const userByEmail = req.body.email;
+  const user = await User.find({email:userByEmail})
+
+  try{
+    if(user.length ===0){
+      res.status(400).send("somthing went wrong")
+    }else{
+      res.send(user);
+    }
+  }catch(err){
+    res.status(400).send("somthing went wrong : "+ err.message)
+  }
+})
+
+//GET-FEED_API: feed 
+app.get("/feed",async(req,res)=>{
+  const feed = await User.find({});
+
+  try{
+    if(feed.length===0){
+      res.status(400).send("somthing went wrong ")
+    }else{
+      res.send(feed)
+    }
+
+  }catch(err){
+    res.status(400).send("something went wrong:"+err.message)
+  }
+})
+
 
 
  connectDB().then(()=>{
